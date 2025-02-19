937f88eb34b361067237bd1a9e835663a423e61a
nans-moreno <nans.moreno@laplateforme.io>
2025-02-19 14:07:49 +0100

    pokedex
    pokedex

---
+++ game.py
@@ -0,0 +1,51 @@
+import pygame
+import random
+import sys
+from pokemon import Pokemon
+from battle import Battle
+from main_menu import MainMenu
+
+class Game:
+    def __init__(self):
+        self.pokemons = self.load_pokemons("pokemons.json")
+        self.main_menu = MainMenu()
+
+    def load_pokemons(self, filepath):
+        """Load Pokémon data from a JSON file."""
+        import json
+        with open(filepath) as file:
+            data = json.load(file)
+
+        pokemons = []
+        for p in data["pokemon"]:
+            pokemons.append(Pokemon(
+                id=p["id"],
+                name=p["name"],
+                base_experience=p["base_experience"],
+                types=p["types"],
+                stats=p["stats"],
+                abilities=p["abilities"],
+                moves=p["moves"],
+                sprites=p["sprites"]
+            ))
+
+        return pokemons
+
+    def start_battle(self):
+        opponent_pokemon = random.choice(self.pokemons)
+        battle = Battle(available_pokemon=self.pokemons, opponent_pokemon=opponent_pokemon)
+        battle.start_battle()
+
+    def run(self):
+        while True:
+            selected_option = self.main_menu.show()
+
+            if selected_option == "start":
+                self.start_battle()
+            elif selected_option == "quit":
+                pygame.quit()
+                sys.exit()
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
